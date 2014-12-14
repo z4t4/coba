1220d6744fb706798a7a7f5d0ef8a2888b0551dc
n4dr4 <nadraririruru@gmail.com>
2014-12-14 16:24:21 +0700

    komentar aku aja

---
+++ Arraymudah.java
@@ -0,0 +1,11 @@
+public class Arraymudah
+{
+	public static void main(String[] args)
+	{
+		int[] ages = new int[10];
+		for(int i=1;i<10;i++)
+		{
+			System.out.println(i+" Tambah "+i+"="+(i+i));
+		}
+	}
+}
